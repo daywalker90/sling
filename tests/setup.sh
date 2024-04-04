38d088f10bbf58ded5610f0cefe79ffafa918916
daywalker90 <8257956+daywalker90@users.noreply.github.com>
2024-04-04 21:28:52 +0200

    ci: upgrade ci to support macos and automated releases

--- tests/setup.sh
+++ tests/setup.sh
@@ -5,41 +5,64 @@ script_dir=$(dirname -- "$(readlink -f -- "$0")")
 
 cargo_toml_path="$script_dir/../Cargo.toml"
 
-# Use grep and awk to extract the version
+# Use grep and awk to extract the name and version
+name=$(awk -F'=' '/^\[package\]/ { in_package = 1 } in_package && /name/ { gsub(/[" ]/, "", $2); print $2; exit }' "$cargo_toml_path")
 version=$(awk -F'=' '/^\[package\]/ { in_package = 1 } in_package && /version/ { gsub(/[" ]/, "", $2); print $2; exit }' "$cargo_toml_path")
 
-get_architecture() {
+get_platform_file_end() {
     machine=$(uname -m)
+    kernel=$(uname -s)
 
-    case $machine in
-        x86_64)
-            echo 'x86_64-linux-gnu'
+    case $kernel in
+        Darwin)
+            echo 'universal-apple-darwin.zip'
             ;;
-        armv7l)
-            echo 'armv7-linux-gnueabihf'
-            ;;
-        aarch64)
-            echo 'aarch64-linux-gnu'
+        Linux)
+            case $machine in
+                x86_64)
+                    echo 'x86_64-linux-gnu.tar.gz'
+                    ;;
+                armv7l)
+                    echo 'armv7-linux-gnueabihf.tar.gz'
+                    ;;
+                aarch64)
+                    echo 'aarch64-linux-gnu.tar.gz'
+                    ;;
+                *)
+                    echo "No self-compiled binary found and unsupported release-architecture: $machine" >&2
+                    exit 1
+                    ;;
+            esac
             ;;
         *)
-            echo "No self-compiled binary found and unsupported release-architecture: $machine" >&2
+            echo "No self-compiled binary found and unsupported OS: $kernel" >&2
             exit 1
             ;;
     esac
 }
-architecture=$(get_architecture)
+platform_file_end=$(get_platform_file_end)
 
-github_url="https://github.com/daywalker90/sling/releases/download/v$version/sling-v$version-$architecture.tar.gz"
+github_url="https://github.com/daywalker90/$name/releases/download/v$version/$name-v$version-$platform_file_end"
 
 
-# Download the file using curl
-if ! curl -L "$github_url" -o "$script_dir/sling-v$version-$architecture.tar.gz"; then
+# Download the archive using curl
+if ! curl -L "$github_url" -o "$script_dir/$name-v$version-$platform_file_end"; then
     echo "Error downloading the file from $github_url" >&2
     exit 1
 fi
 
-# Extract the contents using tar
-if ! tar -xzvf "$script_dir/sling-v$version-$architecture.tar.gz" -C "$script_dir"; then
-    echo "Error extracting the contents of sling-v$version-$architecture.tar.gz" >&2
+# Extract the contents
+if [[ $archive_file == *.tar.gz ]]; then
+    if ! tar -xzvf "$script_dir/$name-v$version-$platform_file_end" -C "$script_dir"; then
+        echo "Error extracting the contents of $name-v$version-$platform_file_end" >&2
+        exit 1
+    fi
+elif [[ $archive_file == *.zip ]]; then
+    if ! unzip "$script_dir/$name-v$version-$platform_file_end" -C "$script_dir"; then
+        echo "Error extracting the contents of $name-v$version-$platform_file_end" >&2
+        exit 1
+    fi
+else
+    echo "Unknown archive format or unsupported file extension: $archive_file" >&2
     exit 1
 fi
